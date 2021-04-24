9d82466afac522cdc8e4420c5bd4c85820dff332
Muhammad Adil <adilafsar99@gmail.com>
2021-04-25 04:59:31 +0500

    index and app files of js-assignment 1 uploaded

---
+++ app.js
@@ -0,0 +1,23 @@
+//Alert for user.
+alert("Hello User!")
+
+//Mr Bean alert
+alert("Hello, Mr Bean.\nHow are you?")
+
+//message variable script
+var message
+message = "Hello World"
+alert(message)
+
+//book variable script
+var book = "A smarter way to learn JavaScript"
+document.write(book)
+document.write("<br /><br />")
+
+//age variable script
+var age = 20
+alert("I am " + age + " years old.")
+
+//birthYear variable script
+var birthYear = 2001
+document.write("I was born in " + birthYear + ".")
