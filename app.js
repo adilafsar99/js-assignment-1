81721dc32ed8f50834c930b765547a1c4498eee6
Muhammad Adil <adilafsar99@gmail.com>
2021-04-25 13:15:02 +0500

    Create app.js

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
+var taskNumber = +prompt("Enter the task number to run the task you want to check: (From 1 to 6)");
+if (taskNumber === 1) {
+
 //Alert for user.
 alert("Hello User!")
 
+}
+
+else if (taskNumber === 2) {
+
 //Mr Bean alert
 alert("Hello, Mr Bean.\nHow are you?")
+
+}
+
+else if (taskNumber === 3) {
 
 //message variable script
 var message
 message = "Hello World"
 alert(message)
 
+}
+
+else if (taskNumber === 4) {
+
 //book variable script
 var book = "A smarter way to learn JavaScript"
 document.write(book)
 document.write("<br /><br />")
 
+}
+
+else if (taskNumber === 5) {
+
 //age variable script
 var age = 20
 alert("I am " + age + " years old.")
 
+}
+
+else if (taskNumber === 6) {
+
 //birthYear variable script
 var birthYear = 2001
 document.write("I was born in " + birthYear + ".")
+
+}
+
+else {
+alert("Invalid input!);
+}
