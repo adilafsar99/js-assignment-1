e9cfa76e10c09a40b5da0a372e94165377873293
Muhammad Adil <adilafsar99@gmail.com>
2021-04-25 14:12:46 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,51 +2,51 @@ var taskNumber = +prompt("Enter the task number to run the task you want to chec
 if (taskNumber === 1) {
 
 //Alert for user.
-alert("Hello User!");
+   alert("Hello User!");
 
 }
 
 else if (taskNumber === 2) {
 
 //Mr Bean alert
-alert("Hello, Mr Bean.\nHow are you?");
+   alert("Hello, Mr Bean.\nHow are you?");
 
 }
 
 else if (taskNumber === 3) {
 
 //message variable script
-var message;
-message = "Hello World";
-alert(message);
+   var message;
+   message = "Hello World";
+   alert(message);
 
 }
 
 else if (taskNumber === 4) {
 
 //book variable script
-var book = "A smarter way to learn JavaScript";
-document.write(book);
-document.write("<br /><br />");
+   var book = "A smarter way to learn JavaScript";
+   document.write(book);
+   document.write("<br /><br />");
 
 }
 
 else if (taskNumber === 5) {
 
 //age variable script
-var age = 20;
-alert("I am " + age + " years old.");
+   var age = 20;
+   alert("I am " + age + " years old.");
 
 }
 
 else if (taskNumber === 6) {
 
 //birthYear variable script
-var birthYear = 2001;
-document.write("I was born in " + birthYear + ".");
+   var birthYear = 2001;
+   document.write("I was born in " + birthYear + ".");
 
 }
 
 else {
-alert("Invalid input!);
+   alert("Invalid input!);
 }
